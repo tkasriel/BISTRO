ff212486774a5231706537b2849ea5db2b6b1d52
tkasriel <71290251+tkasriel@users.noreply.github.com>
2021-04-02 09:58:55 -0700

    cordonConvert now useable

--- SF_light/scripts/cordonConvert.py
+++ SF_light/scripts/cordonConvert.py
@@ -1,55 +1,57 @@
 import utm, math, json
 # Small script to convert cordon params into coords.
-INPUT_FILE = "../input_files/circle_params.txt"
+# INPUT_FILE = "../input_files/circle_params.txt"
 OUTPUT_FILE = "../output_files/circle_params.json"
 
 # Region for UTM-WGS84 > lat/long conversion
-ZONE_NUM = 10
+ZONE_NUM = 14
 ZONE_LETTER = "N"
+def runConvert(INPUT_FILE):
+    coords = []
+    with open (INPUT_FILE, "r") as fileIn:
+        coords = fileIn.readline().split(',')[:-1] # circle_params is a single line
 
-coords = []
-with open (INPUT_FILE, "r") as fileIn:
-    coords = fileIn.readline().split(',')[:-1] # circle_params is a single line
-
-# We need to make it into geoJSON in order for kepler to accept it
-outputJSON = {}
-outputJSON["type"] = "FeatureCollection"
-outputJSON["features"] = []
+    # We need to make it into geoJSON in order for kepler to accept it
+    outputJSON = {}
+    outputJSON["type"] = "FeatureCollection"
+    outputJSON["features"] = []
 
 
-for i in range(0, len(coords), 4): # X, Y, Rad, Cost
-    # Extract values:
-    x = float(coords[i].split(":")[1])
-    y = float(coords[i+1].split(":")[1])
-    r = float(coords[i+2].split(":")[1])
-    p = float(coords[i+3].split(":")[1])
-    center = (x, y)
-    side = (x+r, y) # We'll need to use this to get the radius of the circle
+    for i in range(0, len(coords), 4): # X, Y, Rad, Cost
+        # Extract values:
+        x = float(coords[i].split(":")[1])
+        y = float(coords[i+1].split(":")[1])
+        r = float(coords[i+2].split(":")[1])
+        p = float(coords[i+3].split(":")[1])
+        center = (x, y)
+        side = (x+r, y) # We'll need to use this to get the radius of the circle
+        
+        centerOut = utm.to_latlon(center[0], center[1], ZONE_NUM, ZONE_LETTER)
+        sideOut = utm.to_latlon(side[0], side[1], ZONE_NUM, ZONE_LETTER)
 
-    centerOut = utm.to_latlon(center[0], center[1], ZONE_NUM, ZONE_LETTER)
-    sideOut = utm.to_latlon(side[0], side[1], ZONE_NUM, ZONE_LETTER)
+        # We'll fake a circle by making a regular N-sided polygon
+        sides = 20
+        pts = []
+        radius = math.sqrt((sideOut[0] - centerOut[0]) ** 2 + (sideOut[1] - centerOut[1]) ** 2)
+        for j in range(sides+1):
+            lon = centerOut[0] + radius * math.cos(j * 2 * math.pi / sides)
+            lat = centerOut[1] + radius * math.sin(j * 2 * math.pi / sides)
+            pts.append((lat, lon))
+        
+        # Add circle to output dict
+        outputJSON["features"].append({})
+        poly = outputJSON["features"][-1]
+        poly["type"] = "Feature"
+        poly["geometry"] = {}
+        poly["properties"] = {}
+        poly["properties"]["cost"] = p
 
-    # We'll fake a circle by making a regular N-sided polygon
-    sides = 20
-    pts = []
-    radius = sideOut[0] - centerOut[0]
-    for i in range(sides):
-        lon = round(centerOut[0] + radius * (math.cos(i * (360 / sides))), 6)
-        lat = round(centerOut[1] + radius * (math.sin(i * (360 / sides))), 6)
+        # Geometry for circle
+        geo = poly["geometry"]
+        geo["type"] = "Polygon"
+        geo["coordinates"] = [pts]
 
-        pts.append((lat, lon))
-    
-    outputJSON["features"].append({})
-    poly = outputJSON["features"][-1]
-    poly["type"] = "Feature"
-    poly["geometry"] = {}
-    poly["properties"] = {}
-    poly["properties"]["cost"] = p
 
-    
-    geo = poly["geometry"]
-    geo["type"] = "Polygon"
-    geo["coordinates"] = [pts]
-
-with open(OUTPUT_FILE, "w") as fileOut:
-    fileOut.write(json.dumps(outputJSON))
+    with open(OUTPUT_FILE, "w") as fileOut:
+        fileOut.write(json.dumps(outputJSON))
+    return OUTPUT_FILE
